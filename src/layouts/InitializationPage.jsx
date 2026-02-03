cdb00e167ff4a02dbe85b323ded8bb9433ffd37f
Praveen7892 <praveen@factree.ai>
2026-02-03 17:25:13 +0530

    working on aoi bug

--- src/layouts/InitializationPage.jsx
+++ src/layouts/InitializationPage.jsx
@@ -47,7 +47,23 @@ const InitializationPage = ({
             selectedCameras={selectedCameras}
             onCardClick={handleCardClick}
             onCheckboxClick={toggleCameraSelection}
-            // onSaveAOI={}
+                        onSaveAOI={(aoi) => {
+              setSelectedCameras((prev) => {
+                const safePrev = Array.isArray(prev) ? prev : [];
+
+                const exists = safePrev.find((cam) => cam.id === activeCamera.id);
+
+                if (!exists) {
+                  return [...safePrev, { ...activeCamera, aoi }];
+                }
+
+                return safePrev.map((cam) =>
+                  cam.id === activeCamera.id ? { ...cam, aoi } : cam,
+                );
+              });
+
+              setActiveCamera((prev) => ({ ...prev, aoi }));
+            }}
           />
         </div>
 
